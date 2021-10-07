185db8e34912016a48a9bf234a4cced22cdd5d75
Minny27 <dltmdals0608@naver.com>
2021-10-07 17:32:06 +0900

    [#6]Feat: 커스텀 탭바 구현 - 컬렉션 뷰, 셀 생성 - 이미지 생성

--- JoinUs/Controller/ViewController.swift
+++ JoinUs/Controller/ViewController.swift
@@ -12,57 +12,87 @@ private let teamTabbarId = "teamTabbarCollectionViewCell"
 class ViewController: UIViewController {
     
     // MARK: - Properties
+    
+    let teamTabbarViewmodel = TeamTabbarViewModel()
+    
     let homeButton: UIButton = {
         let button = UIButton()
         button.setTitle("Join Us", for: .normal)
         button.setTitleColor(.black, for: .normal)
-        button.titleLabel?.font = .boldSystemFont(ofSize: 22)
+        button.titleLabel?.font = .boldSystemFont(ofSize: 45)
         button.titleLabel?.textAlignment = .center
         return button
     }()
     
     let teamTabbarCollectionView: UICollectionView = {
-        let collectionView = UICollectionView()
+        let flowLayout = UICollectionViewFlowLayout()
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: flowLayout)
+        collectionView.backgroundColor = .white
         return collectionView
     }()
     
     // MARK: - LifeCycle
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        view.addSubview(homeButton)
         
         configureUI()
     }
     
     func configureUI() {
+        view.addSubview(homeButton)
+        view.addSubview(teamTabbarCollectionView)
+        
+        teamTabbarCollectionView.register(TeamTabbarCollectionViewCell.self, forCellWithReuseIdentifier: teamTabbarId)
+        teamTabbarCollectionView.dataSource = self
+        teamTabbarCollectionView.delegate = self
+        
         homeButton.translatesAutoresizingMaskIntoConstraints = false
         teamTabbarCollectionView.translatesAutoresizingMaskIntoConstraints = false
         
-        homeButton.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
-        homeButton.leftAnchor.constraint(equalTo: view.leftAnchor).isActive = true
-        homeButton.rightAnchor.constraint(equalTo: view.rightAnchor).isActive = true
-        homeButton.heightAnchor.constraint(equalToConstant: 250).isActive = true
+        NSLayoutConstraint.activate([
+            homeButton.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            homeButton.leftAnchor.constraint(equalTo: view.leftAnchor),
+            homeButton.rightAnchor.constraint(equalTo: view.rightAnchor),
+            homeButton.heightAnchor.constraint(equalToConstant: 80),
+            
+            teamTabbarCollectionView.topAnchor.constraint(equalTo: homeButton.bottomAnchor),
+            teamTabbarCollectionView.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 10),
+            teamTabbarCollectionView.rightAnchor.constraint(equalTo: view.rightAnchor, constant: -10),
+            teamTabbarCollectionView.heightAnchor.constraint(equalToConstant: 100),
+//            teamTabbarCollectionView.contentInset.left(
+        ])
         
-        teamTabbarCollectionView.topAnchor.constraint(equalTo: homeButton.topAnchor, constant: 50).isActive = true
-        teamTabbarCollectionView.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 20).isActive = true
-        teamTabbarCollectionView.rightAnchor.constraint(equalTo: view.rightAnchor, constant: 20).isActive = true
-        teamTabbarCollectionView.heightAnchor.constraint(equalToConstant: 100).isActive = true
-        
-        teamTabbarCollectionView.register(teamTabbarCollectionViewCell.self, forCellWithReuseIdentifier: teamTabbarId)
     }
 }
 
-//extension ViewController: UICollectionViewDataSource {
-//    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-//        return 10
-//    }
-//    
-//    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-//        <#code#>
-//    }
-//}
+extension ViewController: UICollectionViewDataSource {
+    override func size(forChildContentContainer container: UIContentContainer, withParentContainerSize parentSize: CGSize) -> CGSize {
+        return CGSize(width: 100, height: 30)
+    }
+    
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: teamTabbarId, for: indexPath) as? TeamTabbarCollectionViewCell else {
+            return UICollectionViewCell()
+        }
+        
+        let teamInfo = teamTabbarViewmodel.teamInfo(at: indexPath.row)
+        cell.update(teamInfo: teamInfo)
+        return cell
+    }
+}
 
 extension ViewController: UICollectionViewDelegate {
     
+}
+
+extension ViewController: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: 100, height: 50)
+    }
 }
--- JoinUs/View/teamTabbarCollectionViewCell.swift
+++ JoinUs/View/teamTabbarCollectionViewCell.swift
@@ -7,6 +7,52 @@
 
 import UIKit
 
-class teamTabbarCollectionViewCell: UICollectionViewCell {
+final class TeamTabbarCollectionViewCell: UICollectionViewCell {
+    let teamLabel: UILabel = {
+        let label = UILabel()
+        label.font = .boldSystemFont(ofSize: 18)
+        label.textAlignment = .center
+        return label
+    }()
     
+    let teamImageView: UIImageView = {
+        let imageView = UIImageView()
+        return imageView
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        configueUI()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func configueUI() {
+        contentView.addSubview(teamLabel)
+        contentView.addSubview(teamImageView)
+        
+        teamLabel.translatesAutoresizingMaskIntoConstraints = false
+        teamImageView.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            teamImageView.centerYAnchor.constraint(equalTo: contentView.centerYAnchor),
+            teamImageView.leftAnchor.constraint(equalTo: contentView.leftAnchor,
+                                                constant: (contentView.frame.width - 20 - 5 - 70) / 2),
+            teamImageView.widthAnchor.constraint(equalToConstant: 20),
+            teamImageView.heightAnchor.constraint(equalToConstant: 20),
+            
+            teamLabel.centerYAnchor.constraint(equalTo: contentView.centerYAnchor),
+            teamLabel.leftAnchor.constraint(equalTo: teamImageView.rightAnchor, constant: 5),
+            teamLabel.widthAnchor.constraint(equalToConstant: 70),
+            teamLabel.heightAnchor.constraint(equalToConstant: 20)
+        ])
+    }
+    
+    func update(teamInfo: TeamModel) {
+        teamLabel.text = teamInfo.teamName
+        teamImageView.image = UIImage(named: teamInfo.teamImageName)
+    }
 }
