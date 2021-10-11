179866b2278b9d0b251064b0ca6b1a83bf47eafd
Minny27 <dltmdals0608@naver.com>
2021-10-11 18:20:17 +0900

    Rafactor: 띄어 쓰기 제거

--- JoinUs/ViewModel/TeamTabbarViewModel.swift
+++ JoinUs/ViewModel/TeamTabbarViewModel.swift
@@ -10,7 +10,7 @@ import Foundation
 final class TeamTabbarViewModel {
     let team: [TeamModel] = [
         TeamModel(teamName: "담원 기아", teamImageName: "1.jpg"),
-        TeamModel(teamName: "한화 생명", teamImageName: "2.jpg"),
+        TeamModel(teamName: "한화생명", teamImageName: "2.jpg"),
         TeamModel(teamName: "T1", teamImageName: "3.jpg"),
         TeamModel(teamName: "젠지", teamImageName: "4.jpg"),
         TeamModel(teamName: "농심", teamImageName: "5.jpg"),
