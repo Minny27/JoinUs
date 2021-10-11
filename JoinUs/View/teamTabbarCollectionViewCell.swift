b01920f2802660b38674792b42d03dacb761d05c
Minny27 <dltmdals0608@naver.com>
2021-10-11 18:22:45 +0900

    [#8]Feat: 팀마다 라벨 크기 다르게 구현

--- JoinUs/View/teamTabbarCollectionViewCell.swift
+++ JoinUs/View/teamTabbarCollectionViewCell.swift
@@ -8,29 +8,34 @@
 import UIKit
 
 final class TeamTabbarCollectionViewCell: UICollectionViewCell {
-    let teamLabel: UILabel = {
-        let label = UILabel()
-        label.font = .boldSystemFont(ofSize: 18)
-        label.textAlignment = .center
-        return label
-    }()
+    let teamLabelWidth: [CGFloat] = [70, 65, 25, 35, 35, 70, 65, 50, 30, 45]
     
     let teamImageView: UIImageView = {
         let imageView = UIImageView()
         return imageView
     }()
     
+    let teamLabel: UILabel = {
+        let label = UILabel()
+        label.font = .boldSystemFont(ofSize: 18)
+        label.textAlignment = .left
+        return label
+    }()
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
         
-        configueUI()
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
     
-    func configueUI() {
+    func configureUI(index: Int?, width: CGFloat?) {
+        if index  == nil || width == nil {
+            return
+        }
+        
         contentView.addSubview(teamLabel)
         contentView.addSubview(teamImageView)
         
@@ -39,14 +44,12 @@ final class TeamTabbarCollectionViewCell: UICollectionViewCell {
         
         NSLayoutConstraint.activate([
             teamImageView.centerYAnchor.constraint(equalTo: contentView.centerYAnchor),
-            teamImageView.leftAnchor.constraint(equalTo: contentView.leftAnchor,
-                                                constant: (contentView.frame.width - 20 - 5 - 70) / 2),
+            teamImageView.leftAnchor.constraint(equalTo: contentView.leftAnchor, constant: (width! - 20 - 5 - teamLabelWidth[index!])),
             teamImageView.widthAnchor.constraint(equalToConstant: 20),
             teamImageView.heightAnchor.constraint(equalToConstant: 20),
             
             teamLabel.centerYAnchor.constraint(equalTo: contentView.centerYAnchor),
             teamLabel.leftAnchor.constraint(equalTo: teamImageView.rightAnchor, constant: 5),
-            teamLabel.widthAnchor.constraint(equalToConstant: 70),
             teamLabel.heightAnchor.constraint(equalToConstant: 20)
         ])
     }
