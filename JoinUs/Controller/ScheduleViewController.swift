1a7caf22fe090e01576d0b2d10f7025d1821286f
Minny27 <dltmdals0608@naver.com>
2021-10-15 14:04:15 +0900

    [#2]Feat: 5개의 탭 생성 - 홈, 일정, 순위, 뉴스, 선수 탭 생성 - ViewController 4개 생성

---
+++ JoinUs/Controller/ScheduleViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ScheduleViewController.swift
+//  JoinUs
+//
+//  Created by SeungMin on 2021/10/15.
+//
+
+import UIKit
+
+class ScheduleViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- JoinUs/Controller/TabbarController.swift
+++ JoinUs/Controller/TabbarController.swift
@@ -10,8 +10,17 @@ import UIKit
 class TabbarController: UITabBarController {
 
     // MARK: - Properies
-    let tabOne = HomeViewController()
-    let tabOneItem = UITabBarItem(title: "HOME", image: UIImage(systemName: "homekit"), selectedImage: UIImage(systemName: "homekit"))
+    let homeTab = HomeViewController()
+    let scheduleTab = ScheduleViewController()
+    let rankTab = RankViewController()
+    let newsTab = NewsViewController()
+    let playerTab = PlayerViewController()
+    
+    let homeTabItem = UITabBarItem(title: "Home", image: UIImage(systemName: "homekit"), selectedImage: UIImage(systemName: "homekit"))
+    let scheduleTabItem = UITabBarItem(title: "Schedule", image: UIImage(systemName: "calendar"), selectedImage: UIImage(systemName: "calendar"))
+    let rankingTabItem = UITabBarItem(title: "Rank", image: UIImage(systemName: "crown"), selectedImage: UIImage(systemName: "crown"))
+    let newsTabItem = UITabBarItem(title: "News", image: UIImage(systemName: "newspaper"), selectedImage: UIImage(systemName: "newspaper"))
+    let playerTabItem = UITabBarItem(title: "Player", image: UIImage(systemName: "person"), selectedImage: UIImage(systemName: "person"))
     
     // MARK: - LifeCycle
     
@@ -20,10 +29,14 @@ class TabbarController: UITabBarController {
         
         configureUI()
 
-        self.viewControllers = [tabOne]
+        self.viewControllers = [homeTab, scheduleTab, rankTab, newsTab, playerTab]
     }
     
     func configureUI() {
-        tabOne.tabBarItem = tabOneItem
+        homeTab.tabBarItem = homeTabItem
+        scheduleTab.tabBarItem = scheduleTabItem
+        rankTab.tabBarItem = rankingTabItem
+        newsTab.tabBarItem = newsTabItem
+        playerTab.tabBarItem = playerTabItem
     }
 }
