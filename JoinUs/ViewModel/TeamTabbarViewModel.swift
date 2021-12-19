eac7d23faac70968685392be73454f21d8d29591
Minny27 <dltmdals0608@naver.com>
2021-12-19 23:43:04 +0900

    [#63]Refactor: 파일 수정 - TeamTabbarViewModel, TeamImage 팀 이미지 이름 변경
    - TeamPlayerCollectionViewCell
    playerGameIdLabel 텍스트 크기 13으로 변경
    
    - NewsViewController
    화면 전환 함수 변경
    present() -> pushViewController()
    
    - NewsDetailViewController
    네비게이션 바 보임
    back 버튼 클릭 시, 네비게이션 바 숨김

--- JoinUs/ViewModel/TeamTabbarViewModel.swift
+++ JoinUs/ViewModel/TeamTabbarViewModel.swift
@@ -9,16 +9,16 @@ import Foundation
 
 final class TeamTabbarViewModel{
     let teamList: [Team] = [
-        Team("담원기아", "1.jpg"),
-        Team("한화생명", "2.jpg"),
-        Team("T1", "3.jpg"),
-        Team("젠지", "4.jpg"),
-        Team("농심", "5.jpg"),
-        Team("리브샌박", "6.jpg"),
-        Team("아프리카", "7.jpg"),
-        Team("프레딧", "8.jpg"),
-        Team("KT", "9.jpg"),
-        Team("DRX", "10.jpg"),
+        Team("담원기아", "DK.jpg"),
+        Team("한화생명", "HLE.jpg"),
+        Team("T1", "T1.jpg"),
+        Team("젠지", "GenG.jpg"),
+        Team("농심", "NS.jpg"),
+        Team("리브샌박", "Liiv.jpg"),
+        Team("아프리카", "Afreeca.jpg"),
+        Team("프레딧", "Fredit.jpg"),
+        Team("KT", "KT.jpg"),
+        Team("DRX", "DRX.jpg"),
     ]
     
     var countTeamList: Int{
