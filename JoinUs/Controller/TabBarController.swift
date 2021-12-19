09adf9af73c6d9cd337da85229fd58e21cf73607
Minny27 <dltmdals0608@naver.com>
2021-12-19 23:34:17 +0900

    [#67]Refactor: 코드 정리 - 탭바 희미하게 보이는 문제 해결

--- JoinUs/Controller/TabBarController.swift
+++ JoinUs/Controller/TabBarController.swift
@@ -7,38 +7,39 @@
 
 import UIKit
 
-class TabbarController: UITabBarController {
-
-    // MARK: - Properies
-    let homeTab = HomeViewController()
-    let scheduleTab = ScheduleViewController()
-    let standingsTab = StandingsViewController()
-    let newsTab = NewsViewController()
-    let playerTab = PlayerViewController()
-    
-    let homeTabItem = UITabBarItem(title: "Home", image: UIImage(systemName: "house"), selectedImage: UIImage(systemName: "house"))
-    let scheduleTabItem = UITabBarItem(title: "Schedule", image: UIImage(systemName: "calendar"), selectedImage: UIImage(systemName: "calendar"))
-    let standingsTabItem = UITabBarItem(title: "Rank", image: UIImage(systemName: "crown"), selectedImage: UIImage(systemName: "crown"))
-    let newsTabItem = UITabBarItem(title: "News", image: UIImage(systemName: "newspaper"), selectedImage: UIImage(systemName: "newspaper"))
-    let playerTabItem = UITabBarItem(title: "Player", image: UIImage(systemName: "person"), selectedImage: UIImage(systemName: "person"))
-    
-    // MARK: - LifeCycle
-    
+class TabBarController: UITabBarController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
         configureUI()
-
-        self.viewControllers = [homeTab, scheduleTab, standingsTab, newsTab, playerTab]
     }
     
     func configureUI() {
-        view.backgroundColor = .white
+        tabBar.tintColor = .systemBlue
+        tabBar.unselectedItemTintColor = .systemGray
+        tabBar.isTranslucent = false
         
-        homeTab.tabBarItem = homeTabItem
-        scheduleTab.tabBarItem = scheduleTabItem
-        standingsTab.tabBarItem = standingsTabItem
-        newsTab.tabBarItem = newsTabItem
-        playerTab.tabBarItem = playerTabItem
+        view.backgroundColor = .white
+        self.viewControllers = [
+            createTabBarItem(title: "Home", image: "house", viewController: HomeViewController()),
+            createTabBarItem(title: "Schedule", image: "calendar", viewController: ScheduleViewController()),
+            createTabBarItem(title: "Rank", image: "crown", viewController: StandingsViewController()),
+            createTabBarItem(title: "News", image: "newspaper", viewController: NewsViewController()),
+            createTabBarItem(title: "Player", image: "person", viewController: PlayerViewController()),
+        ]
+    }
+    
+    func createTabBarItem(
+        title: String,
+        image: String,
+        viewController: UIViewController
+    ) -> UINavigationController {
+        let navigationController = UINavigationController(rootViewController: viewController)
+        navigationController.tabBarItem.title = title
+        navigationController.tabBarItem.image = UIImage(systemName: image)
+        navigationController.isNavigationBarHidden = true
+        navigationController.isToolbarHidden = true
+        
+        return navigationController
     }
 }
