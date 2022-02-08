22477e155f9977188fe89158ebb924ad8de7c036
Minny27 <dltmdals0608@naver.com>
2022-02-08 22:54:46 +0900

    [#36]Feat: 일정을 요청하고 일정 테이블 뷰를 구현하기 위한 view model 구현 - 월별 경기 일정 리스트 구현 - 월마다 일별을 구분하기 위해 set 구현 - 월별로 데이터가 있는지 확인하는 bool array 구현 - 두 페이지의 데이터를 요청하면서 set에 일정이 있으면 일정 한 번만 추가 - 없으면 두번 추가 - year, month 추가

--- JoinUs/Source/Home/ViewModel/LeagueScheduleTableViewModel.swift
+++ JoinUs/Source/Home/ViewModel/LeagueScheduleTableViewModel.swift
@@ -9,31 +9,46 @@ import Foundation
 
 final class LeagueScheduleTableViewModel {
     private let dateFormatter = DateFormatter()
+    private let pageNumberList: [String] = ["1", "2"]
+    private var leagueType: RequestLeagueType
+    private var numberOfMonthlyMatches: [Int] = [0,0,0,0,0,0,0,0,0,0,0,0,0]
+    private var setOfDate: [String: Set<String>] = [
+        "01": Set<String>(), "02": Set<String>(), "03": Set<String>(), "04": Set<String>(),
+        "05": Set<String>(), "06": Set<String>(), "07": Set<String>(), "08": Set<String>(),
+        "09": Set<String>(), "10": Set<String>(), "11": Set<String>(), "12": Set<String>()
+    ]
+    
     var todayScheduleList: Observable<[LeagueScheduleTableViewCellModel]> = Observable([])
     var monthScheduleList: Observable<[LeagueScheduleTableViewCellModel]> = Observable([])
-    var dayOfMonthSchedule: [String: [LeagueScheduleTableViewCellModel]] = [:]
-    var leagueType: RequestLeagueType
+    var monthlySchedule: Observable<[String: [LeagueScheduleTableViewCellModel]]> = Observable([
+        "01": [LeagueScheduleTableViewCellModel](), "02": [LeagueScheduleTableViewCellModel](),
+        "03": [LeagueScheduleTableViewCellModel](), "04": [LeagueScheduleTableViewCellModel](),
+        "05": [LeagueScheduleTableViewCellModel](), "06": [LeagueScheduleTableViewCellModel](),
+        "07": [LeagueScheduleTableViewCellModel](), "08": [LeagueScheduleTableViewCellModel](),
+        "09": [LeagueScheduleTableViewCellModel](), "10": [LeagueScheduleTableViewCellModel](),
+        "11": [LeagueScheduleTableViewCellModel](), "12": [LeagueScheduleTableViewCellModel]()
+    ])
     var hasTodayData: Bool = true
-    var hasMonthData: Bool = true
+    var hasMonthData: [Bool] = [true,true,true,true,true,true,true,true,true,true,true,true,true]
     
     var countTodayScheduleList: Int {
         return todayScheduleList.value?.count ?? 0
-    }
-    
-    var countMonthScheduleList: Int {
-        return monthScheduleList.value?.count ?? 0
     }
     
     init(leagueType: RequestLeagueType) {
         self.leagueType = leagueType
     }
     
+    func countMonthlyScheduleList(month: String) -> Int {
+        return monthlySchedule.value?[month]!.count ?? 0 + numberOfMonthlyMatches[Int(month)!]
+    }
+    
     func todayScheduleInfo(at index: Int) -> LeagueScheduleTableViewCellModel? {
         return todayScheduleList.value?[index]
     }
     
-    func monthScheduleInfo(at index: Int) -> LeagueScheduleTableViewCellModel? {
-        return monthScheduleList.value?[index]
+    func monthScheduleInfo(month: String, index: Int) -> LeagueScheduleTableViewCellModel? {
+        return monthlySchedule.value?[month]![index]
     }
     
     func fetchTodayData() {
@@ -49,38 +64,67 @@ final class LeagueScheduleTableViewModel {
     }
     
     func fetchMonthData() {
-        NetworkManger().getScheduleData(scheduleUrl: self.leagueType.totalScheduleUrl) { receivedScheduleModel in
-            self.monthScheduleList.value = receivedScheduleModel.compactMap({ schedule in
-                self.extractScehduleData(schedule: schedule)
-            })
-            
-            if let monthSechduleList = self.monthScheduleList.value {
-                monthSechduleList.sorted { ($1.date, $0.time) < ($0.date, $1.time) }.forEach {
-                    if self.dayOfMonthSchedule[$0.date] == nil {
-                        self.dayOfMonthSchedule[$0.date] = [$0]
+        let todayYear = dateFormatter.dateToString(date: Date(), dateFormat: .year)
+        
+        self.pageNumberList.forEach { pageNumber in
+            RequestLeagueType.pageNumber = pageNumber
+            NetworkManger().getScheduleData(scheduleUrl: self.leagueType.totalScheduleUrl) { receivedScheduleModel in
+                self.monthScheduleList.value = receivedScheduleModel.compactMap({ schedule in
+                    self.extractScehduleData(schedule: schedule)
+                }).sorted(by: { ($0.date, $0.time) < ($1.date, $1.time) })
+                
+                for schedule in self.monthScheduleList.value! {
+                    if todayYear != schedule.year {
+                        break
                     }
-                        
+                    
+                    // 경기가 있는 월에 경기수 증가 시키기
+                    let month = Int(schedule.month)!
+                    self.numberOfMonthlyMatches[month] += 1
+                    
+                    // 월별 경기 넣기
+                    if self.setOfDate[schedule.month]!.contains(schedule.date) {
+                        self.monthlySchedule.value![schedule.month]!.append(schedule)
+                    }
+                    
                     else {
-                        self.dayOfMonthSchedule[$0.date]!.append($0)
+                        self.setOfDate[schedule.month]!.insert(schedule.date)
+                        self.monthlySchedule.value![schedule.month]!.append(schedule)
+                        self.monthlySchedule.value![schedule.month]!.append(schedule)
                     }
                 }
-            }
-            
-            if self.monthScheduleList.value!.count == 0 {
-                self.hasMonthData = false
+                
+//                print(self.numberOfMonthlyMatches)
+//                print(self.monthlySchedule.value!["02"])
+                // 월마다 데이터가 없으면 해당 월 false
+                self.monthlySchedule.value?.forEach({ key, value in
+                    if self.monthlySchedule.value![key]!.count <= 0 {
+                        self.hasMonthData[Int(key)!] = false
+                        print("\(key): \(self.hasMonthData[Int(key)!])")
+                    }
+                })
             }
         }
     }
+        
     
     func extractScehduleData(schedule: ReceivedScheduleModel) -> LeagueScheduleTableViewCellModel {
         let leagueImage = schedule.league.imageUrl
         let league = schedule.league.name
-        let date = self.dateFormatter.dateToString(
-            date: schedule.scheduledAt,
+        let year = dateFormatter.dateToString(
+            date: schedule.originalScheduledAt,
+            dateFormat: .year
+        )
+        let month = dateFormatter.dateToString(
+            date: schedule.originalScheduledAt,
+            dateFormat: .month
+        )
+        let date = dateFormatter.dateToString(
+            date: schedule.originalScheduledAt,
             dateFormat: .date
         )
-        let time = self.dateFormatter.dateToString(
-            date: schedule.scheduledAt,
+        let time = dateFormatter.dateToString(
+            date: schedule.originalScheduledAt,
             dateFormat: .time
         ).replacingOccurrences(of: "-", with: ":")
         let status = schedule.status
@@ -111,6 +155,8 @@ final class LeagueScheduleTableViewModel {
         let scheduleTableViewCellModel = LeagueScheduleTableViewCellModel(
             leagueImage: leagueImage,
             league: league,
+            year: year,
+            month: month,
             date: date,
             time: time,
             status: status,
