0122722d198dab2ffe7196ad1cf953114b2b4141
Minny27 <dltmdals0608@naver.com>
2022-02-13 19:26:14 +0900

    [#80]Feat: 두 페이지 일정 데이터 한 번에 가져오기 - dispatchGroup을 이용해서 한 번에 두 페이지 데이터 가져오기 - dispatchGroup 생성, enter(), leave(), notify() - NSIndexPath objectForKey 에러 해결

--- JoinUs/Network/NetworkManager.swift
+++ JoinUs/Network/NetworkManager.swift
@@ -7,27 +7,25 @@
 
 import Foundation
 
-final class NetworkManger {
-    func getScheduleData(
-        scheduleUrl: URL?,
+final class NetworkManager {
+    func getTodaySchedule(
+        url: URL?,
         completion: @escaping (([ReceivedScheduleModel]) -> Void)
     ) {
-        guard let requestUrl = scheduleUrl else {
+        guard let requestUrl = url else {
             print(NetworkError.invalidUrl)
             return
         }
-        
         let headers = [
             "Accept": "application/json",
             "Authorization": Storage().apiKey
         ]
-        
         let request = NSMutableURLRequest(
             url: requestUrl,
             cachePolicy: .useProtocolCachePolicy,
             timeoutInterval: 10.0
         )
-                
+        
         request.httpMethod = "GET"
         request.allHTTPHeaderFields = headers
         
@@ -35,32 +33,88 @@ final class NetworkManger {
         urlSession.dataTask(
             with: request as URLRequest,
             completionHandler: { (data, response, error) -> Void in
-            guard let responseStatus = response as? HTTPURLResponse, responseStatus.statusCode == 200 else {
-                print(NetworkError.invalidResponse)
+                guard let responseStatus = response as? HTTPURLResponse, responseStatus.statusCode == 200 else {
+                    print(NetworkError.invalidResponse)
+                    return
+                }
                 
+                guard let data = data, error == nil else {
+                    print(NetworkError.invalidData)
+                    return
+                }
+                var scheduleData: [ReceivedScheduleModel]
+                
+                do {
+                    let decoder = JSONDecoder()
+                    decoder.dateDecodingStrategy = .iso8601
+                    decoder.keyDecodingStrategy = .convertFromSnakeCase
+                    scheduleData = try decoder.decode(
+                        [ReceivedScheduleModel].self,
+                        from: data
+                    )
+                    completion(scheduleData)
+                } catch {
+                    print(NetworkError.decodingError)
+                }
+            }
+        ).resume()
+    }
+    
+    func getMonthSchedule(
+        urls: [URL?],
+        completion: @escaping (([ReceivedScheduleModel]) -> Void)
+    ) {
+        let dispatchGroup = DispatchGroup()
+        var scheduleList: [ReceivedScheduleModel] = []
+        
+        urls.forEach { url in
+            dispatchGroup.enter()
+            
+            guard let requestUrl = url else {
+                print(NetworkError.invalidUrl)
                 return
             }
+            let headers = [
+                "Accept": "application/json",
+                "Authorization": Storage().apiKey
+            ]
+            let request = NSMutableURLRequest(
+                url: requestUrl,
+                cachePolicy: .useProtocolCachePolicy,
+                timeoutInterval: 10.0
+            )
             
-            guard let data = data, error == nil else {
-                print(NetworkError.invalidData)
-                
-                return
-            }
+            request.httpMethod = "GET"
+            request.allHTTPHeaderFields = headers
             
-            var scheduleData: [ReceivedScheduleModel]
-            
-            do {
-                let decoder = JSONDecoder()
-                decoder.dateDecodingStrategy = .iso8601
-                decoder.keyDecodingStrategy = .convertFromSnakeCase
-                scheduleData = try decoder.decode(
-                    [ReceivedScheduleModel].self,
-                    from: data
-                )
-                completion(scheduleData)
-            } catch {
-                print(NetworkError.decodingError)
-            }
-        }).resume()
+            let urlSession = URLSession.shared
+            urlSession.dataTask(
+                with: request as URLRequest,
+                completionHandler: { (data, response, error) -> Void in
+                    guard let responseStatus = response as? HTTPURLResponse, responseStatus.statusCode == 200 else {
+                        print(NetworkError.invalidResponse)
+                        return
+                    }
+                    guard let data = data, error == nil else {
+                        print(NetworkError.invalidData)
+                        return
+                    }
+                    
+                    do {
+                        let decoder = JSONDecoder()
+                        decoder.dateDecodingStrategy = .iso8601
+                        decoder.keyDecodingStrategy = .convertFromSnakeCase
+                        scheduleList += try decoder.decode([ReceivedScheduleModel].self, from: data)
+                    } catch {
+                        print(NetworkError.decodingError)
+                    }
+                    dispatchGroup.leave()
+                }
+            ).resume()
+        }
+        
+        dispatchGroup.notify(queue: .global()) {
+            completion(scheduleList)
+        }
     }
 }
